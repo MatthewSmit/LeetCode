533a6c3711dcc99be95388cae3010e6894132c2a
Matthew Smit <programmdude@gmail.com>
2018-10-21 12:35:12 +1300

    Added two more array solutions

---
+++ LeetCode/Array/ContainsDuplicate.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public partial class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        return new HashSet<int>(nums).Count != nums.Length;
+    }
+}
---
+++ LeetCode/Array/SingleNumber.cs
@@ -0,0 +1,12 @@
+﻿public partial class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        var result = 0;
+        foreach (var num in nums)
+        {
+            result ^= num;
+        }
+        return result;
+    }
+}
