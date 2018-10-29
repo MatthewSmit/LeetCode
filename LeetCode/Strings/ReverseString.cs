ed6c3cab948e3ebc4a6b0e8cbf00e1536e8eccba
Matthew Smit <programmdude@gmail.com>
2018-10-29 21:42:37 +1300

    Added reverse string solution.

---
+++ LeetCode/Strings/ReverseString.cs
@@ -0,0 +1,14 @@
+﻿public partial class Solution
+{
+    public string ReverseString(string s)
+    {
+        var c = s.ToCharArray();
+        for (var i = 0; i < c.Length / 2; i++)
+        {
+            var tmp = c[i];
+            c[i] = c[c.Length - i - 1];
+            c[c.Length - i - 1] = tmp;
+        }
+        return new string(c);
+    }
+}
